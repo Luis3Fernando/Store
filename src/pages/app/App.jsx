f480b37a6424f20e1470c70a1f3c2f3110636b3e
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2023-07-29 09:41:43 -0500

    Realizando enrutammiento

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
+import {useRoutes, BrowserRouter} from 'react-router-dom'
 import Home from '../home/Home'
+import Account from '../components/Account'
+import NotFound from '../components/NotFound'
+import Order from '../components/Order'
+import Orders from '../components/Orders'
+import Signin from '../components/Signin'
+
+const AppRoutes = () =>{
+  let routes = useRoutes([
+    { path: '/', element: <Home></Home> },
+    { path: '/my-account', element: <Account></Account> },
+    { path: '/*', element: <NotFound></NotFound> },
+    { path: '/order', element: <Order></Order> },
+    { path: '/orders', element: <Orders></Orders> },
+    { path: '/signin', element: <Signin></Signin> },
+  ])
+
+  return routes
+}
 
 function App() {
-
+  
   return (
     <>
-    <Home></Home>
-     <h1>hola</h1>
+    <BrowserRouter>
+      <AppRoutes></AppRoutes>
+    </BrowserRouter>
     </>
   )
 }
